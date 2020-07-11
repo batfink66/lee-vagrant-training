2e75b7cd09e8f2c4e849e459104b5769170cab58
batfink66 <batfink66@gmail.com>
2020-07-11 14:28:28 +0100

    new files added

---
+++ scripts/centos68-lamp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# update centos but exclude kernel packages 
+yum update -y exclude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld 
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download starter content
+
+service httpd restart
