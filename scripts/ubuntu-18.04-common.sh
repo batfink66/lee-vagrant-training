9cd3b52649db2c09b37badd4e9cd2d94bdf1340b
batfink66 <batfink66@gmail.com>
2020-07-23 06:44:47 +0100

    added hold on kernel updates and netcat

--- scripts/ubuntu-18.04-common.sh
+++ scripts/ubuntu-18.04-common.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # update ubuntu but exclude kernel packages
-apt-get update -y --exclude=kernel
+apt-mark hold linux-image-generic linux-headers-generic
+apt-get update -y
 
 # Tools
-apt-get install -y nano git unzip screen nc telnet openssh-server
+apt-get install -y nano git unzip screen netcat telnet openssh-server
